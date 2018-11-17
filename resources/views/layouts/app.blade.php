16c2fdc18d324d83c748c2f3842587f28fdbdae1
=vayne7 <=littlekign@gmail.com>
2018-11-17 09:44:52 +0800

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Todo example from laravel 5.5</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+
+    <!-- Custom styles for this template -->
+  </head>
+
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+
+  </body>
+</html>
---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+
+
+@section('content')
+
+	<h1>this is title from the todos.index template </h1>
+
+@endsection
